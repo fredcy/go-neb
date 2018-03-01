62028ca9a94fbe079c0626a8d3053eb83da8d8a9
Fred Yankowski <fcy@imsa.edu>
2018-03-01 23:10:55 +0000

    draft new !topic command and badword matching

--- src/github.com/matrix-org/go-neb/services/t3bot/t3bot.go
+++ src/github.com/matrix-org/go-neb/services/t3bot/t3bot.go
@@ -1,9 +1,12 @@
-// Package t3bot implements a Service which t3botes back !commands.
+// Package t3bot implements a Service for Tezos room !commands.
 package t3bot
 
 import (
+        "fmt"
+        "regexp"
 	"strings"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/matrix-org/go-neb/types"
 	"github.com/matrix-org/gomatrix"
 )
@@ -16,6 +19,16 @@ type Service struct {
 	types.DefaultService
 }
 
+var topicMessage = "This would be the <i>topic</i> message."
+
+var topicHTMLMessage = gomatrix.HTMLMessage{
+    MsgType: "m.notice",
+    Body: "This would be the topic message.",
+    Format: "org.matrix.custom.html",
+    FormattedBody: "This would be the <strong>topic message</strong>.",
+}
+
+
 // Commands supported:
 //    !t3bot some message
 // Responds with a notice of "some message".
@@ -27,7 +40,38 @@ func (e *Service) Commands(cli *gomatrix.Client) []types.Command {
 				return &gomatrix.TextMessage{"m.notice", strings.Join(args, " ")}, nil
 			},
 		},
+		types.Command{
+			Path: []string{"topic"},
+			Command: func(roomID, userID string, args []string) (interface{}, error) {
+				//return &gomatrix.TextMessage{"m.notice", topicMessage}, nil
+				return topicHTMLMessage, nil
+			},
+		},
 	}
+}
+
+
+// Match message with bad words. Constuct pattern that it matches only
+// once per message so that respond only once.
+var badwordsRegex = regexp.MustCompile(`(?i:^.*\b(gevers|rumplestiltskin)\b.*$)`)
+
+var badwordsExpand = types.Expansion{
+    Regexp: badwordsRegex,
+
+    Expand: func(roomID, userID string, matches []string) interface{} {
+    	 log.WithFields(log.Fields{"room_id": roomID, "user_id": userID, "matches": matches}).Print("badwords matched")
+	 return &gomatrix.TextMessage{
+	 	"m.notice",
+		fmt.Sprintf("%s used bad words", userID),
+ 	 }
+    },
+}
+
+
+func (s *Service) Expansions(cli *gomatrix.Client) []types.Expansion {
+     return []types.Expansion{
+     	    badwordsExpand,
+     }
 }
 
 func init() {
